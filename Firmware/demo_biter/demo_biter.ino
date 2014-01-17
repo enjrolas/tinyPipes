0477c3e0a3423d4c62b6677c143c0d6c56c2c2aa
mini-muffin <jomg@mit.edu>
2014-01-16 21:39:52 -0500

    added semi-functioning pulse counting PLCC system

---
+++ Firmware/demo_biter/demo_biter.ino
@@ -0,0 +1,59 @@
+#include <avr/interrupt.h>
+
+#define R 5
+#define G 6
+#define B 7
+#define rcv 2
+
+unsigned long stime=0;
+int etime=0;
+int count=0;
+char state=0;
+char nstate=0;
+volatile unsigned long ttime=0;
+volatile unsigned long ltrig=0;
+volatile char wait=0;
+volatile int countr=0;
+
+ISR(INT0_vect)
+{
+  countr++;
+  wait=0;
+}
+
+void setup()
+{
+  EICRA|=0x02; //INT0 falling edge interrupt
+  EIMSK|=0x01; //Enable INTO interrupts
+  Serial.begin(9600);
+  pinMode(rcv, INPUT);
+  stime=millis();
+  ltrig=micros();
+  sei();
+}
+
+void loop()
+{
+ /* nstate=digitalRead(rcv);
+  if(state!=nstate)
+  {
+    state=nstate;
+    if(wait==1)
+    {
+      stime=millis();
+      wait=0;
+    }
+    else
+    {
+      if(state==0)count++;
+    } 
+  }
+  */
+  if(wait==1)stime=millis();
+  if(((millis()-stime)>30)||(millis()<stime))
+  {
+   wait=1;
+   Serial.println(countr, DEC);
+   countr=0; 
+  }
+}
---
+++ Firmware/demo_talker/demo_talker.ino
@@ -0,0 +1,58 @@
+#define R 5
+#define G 6 
+#define B 7
+#define nfet 18
+#define pfet 17
+
+unsigned char battData=0;
+
+void pillowTalk(char data);
+void clock(void);
+void setup()
+{
+  pinMode(R,OUTPUT); //analogWrite(R,d)
+  pinMode(G,OUTPUT); //analogWrite(G,d)
+  pinMode(B,OUTPUT); //can't analogWrite(B,d)
+  pinMode(nfet,OUTPUT);
+  pinMode(pfet,OUTPUT);
+  digitalWrite(nfet,LOW);
+  digitalWrite(pfet,HIGH);
+}
+
+void loop()
+{
+  for(unsigned char y=0;y<255;y++)
+  {
+   pillowTalk(y);
+   delay(30);
+  } 
+}
+
+void pillowTalk(unsigned char data)
+{
+  for(unsigned char x=0;x<255;x++)
+  {
+    if(x<=data)
+    {
+      clock();
+    }
+    else
+    {
+      delayMicroseconds(74);
+    }
+  }
+  clock();
+}
+
+void clock(void)
+{
+  digitalWrite(pfet,LOW);
+  delayMicroseconds(7);
+  digitalWrite(nfet,HIGH);
+  delayMicroseconds(20);
+  digitalWrite(nfet,LOW);
+  delayMicroseconds(7);
+  digitalWrite(pfet,HIGH);
+  delayMicroseconds(20); 
+}
+
